aeae7bdf98a8edcbf76c8b8b832afe8238ee4f92
AEpolyakov <ae.polyakov@mail.ru>
2021-11-08 13:04:53 +0400

    find bug in offers

--- pages/product_page.py
+++ pages/product_page.py
@@ -9,4 +9,4 @@ class ProductPage(BasePage):
         self.solve_quiz_and_get_code()
         prod_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
         prod_name_mess = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_MESSAGES).text
-        assert prod_name in prod_name_mess, 'wrong book added to basket'
+        assert prod_name == prod_name_mess, 'wrong book added to basket'
