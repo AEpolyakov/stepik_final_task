b1040e84e1fcc59f073a78083d02118e25ba3bc1
AEpolyakov <ae.polyakov@mail.ru>
2021-11-08 09:59:10 +0400

    should be login link via try

--- test_main_page.py
+++ test_main_page.py
@@ -6,7 +6,8 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    time.sleep(3)
-    page.go_to_login_page()
-    time.sleep(3)
+    page.should_be_login_link()
+    # time.sleep(3)
+    # page.go_to_login_page()
+    # time.sleep(3)
 
